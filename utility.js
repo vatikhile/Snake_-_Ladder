198327e941cda8b76ba49249c7d33b052c904df3
“vatikhile” <vaibhawatikhile@gmail.com>
2021-01-27 14:56:56 +0530

    update dice c

--- utility.js
+++ utility.js
@@ -9,34 +9,44 @@ class Utility {
     current_position = 0;
 
     rollDice = () => {
+        let dice_count = 0;
+        this.current_position = this.Start_Place;
+        while (this.current_position < this.End_place) {
 
-        var dice = Math.floor(Math.random() * 6) + 1;
-        console.log('Random check dice : ' + dice);
-        let check_option = Math.floor(Math.random() * 3) + 1;
 
-        switch (check_option) {
-            case this.SNAKE:
-                console.log('SNAKE');
-                if (this.current_position - dice >= this.Start_Place) {
-                    this.current_position = this.current_position - dice;
-                }
-                break;
 
-            case this.LADDER:
-                console.log(' LADDER ');
-                if (this.current_position + dice <= this.End_place) {
-                    this.current_position = this.current_position + dice;
-                }
-                break;
 
-            default:
-                console.log('NO PLAY');
-                this.current_position = this.current_position;
-                break;
+            var dice = Math.floor(Math.random() * 6) + 1;
+            console.log('Random check dice : ' + dice);
+            let check_option = Math.floor(Math.random() * 3) + 1;
 
-                
+            switch (check_option) {
+                case this.SNAKE:
+                    console.log('SNAKE ');
+                    if (this.current_position - dice >= this.Start_Place) {
+                        this.current_position = this.current_position - dice;
+                    }
+                    break;
+
+                case this.LADDER:
+                    console.log(' LADDER ');
+                    if (this.current_position + dice <= this.End_place) {
+                        this.current_position = this.current_position + dice;
+                    }
+                    break;
+
+                default:
+                    console.log('NO PLAY');
+                    this.current_position = this.current_position;
+                    break;
+
+
+            }
+            dice_count++;
         }
         console.log(`Current position :${this.current_position}`);
+        console.log('Dice count is :'+dice_count);
+
     }
 
 
