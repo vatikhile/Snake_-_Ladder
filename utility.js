79fcc20bd2b48256d352111f62e7b849577b3eb2
“vatikhile” <vaibhawatikhile@gmail.com>
2021-01-27 14:21:44 +0530

    check Options

--- utility.js
+++ utility.js
@@ -1,13 +1,42 @@
+class Utility {
 
 
-class Utility{
     Start_Place = 0;
-     End_Place = 100;
+    End_place = 100;
+    SNAKE = 1;
+    LADDER = 2;
+    NO_PLAY = 0;
+    current_position = 0;
 
     rollDice = () => {
-        var random_check = Math.floor( Math.random() * 6 ) + 1 ;
-         console.log('Random check dice : '  +random_check);
+
+        var dice = Math.floor(Math.random() * 6) + 1;
+        console.log('Random check dice : ' + dice);
+        let check_option = Math.floor(Math.random() * 3) + 1;
+
+        switch (check_option) {
+            case this.SNAKE:
+                console.log('SNAKE');
+                if (this.current_position - dice >= this.STARTING_POSITION) {
+                    this.current_position = this.current_position - dice;
+                }
+                break;
+
+            case this.LADDER:
+                console.log(' LADDER ');
+                if (this.current_position + dice <= this.ENDING_POSITION) {
+                    this.current_position = this.current_position + dice;
+                }
+                break;
+
+            default:
+                console.log('NO PLAY');
+                this.current_position = this.current_position;
+                break;
+        }
+        console.log(`Current position :${this.current_position}`);
     }
 
+
 }
-module.exports=new Utility()
+module.exports = new Utility();
