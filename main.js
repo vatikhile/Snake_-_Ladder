055bfcb7f1668784de9c9624bb446b7da2afb904
“vatikhile” <vaibhawatikhile@gmail.com>
2021-01-27 08:33:07 +0530

    dice roll

---
+++ main.js
@@ -0,0 +1,4 @@
+
+console.log("WELCOME TO SNAKE AND LADDER");
+const util = require('./utility.js');
+util.rollDice();
--- utility.js
+++ utility.js
@@ -1,5 +1,13 @@
+
+
 class Utility{
     Start_Place = 0;
-    End_Place = 100;
+     End_Place = 100;
+
+    rollDice = () => {
+        var random_check = Math.floor( Math.random() * 6 ) + 1 ;
+         console.log('Random check dice : '  +random_check);
+    }
+
 }
-module.exports=new Utility();
+module.exports=new Utility()
